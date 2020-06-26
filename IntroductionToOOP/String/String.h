2ef8a30f669d887a297891edcce6848dff8c9cfb
httpc <makshttpc@gmail.com>
2020-06-26 21:13:19 +0300

    Separated

---
+++ IntroductionToOOP/String/String.h
@@ -0,0 +1,36 @@
+#pragma once
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+class String;
+String operator+(const String& left, const String& right);
+std::ostream& operator<<(std::ostream& os, const String& obj);
+class String
+{
+	int size;
+	char* str;
+public:
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
+
+	//			Constructors:
+	explicit String(int size = 80);
+	String(const char str[]);
+	String(const String& other);
+	String(String&& other);
+	~String();
+
+	//			Operators:
+	String& operator=(const String& other);
+	String& operator=(String&& other);
+	String& operator+=(const String& other);
+
+	const char& operator[](int i)const;
+	char& operator[](int i);
+
+	//			Methods:
+	void print()const;
+};
