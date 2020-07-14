95416730b1116239d8339a0ed77f2c08da034dfc
httpc <makshttpc@gmail.com>
2020-07-14 17:37:39 +0300

    home

--- Inhertance/Geometry/main.cpp
+++ Inhertance/Geometry/main.cpp
@@ -19,7 +19,6 @@ namespace Geometry
 		virtual double perimeter()const = 0;
 		virtual void draw()const = 0;
 	};
-
 	class Square :public Shape
 	{
 		double side;
@@ -63,7 +62,6 @@ namespace Geometry
 			}
 		}
 	};
-
 	class Rectangle :public Shape
 	{
 		double side1;
@@ -123,6 +121,65 @@ namespace Geometry
 			}
 		}
 	};
+	class Triangle :public Shape
+	{
+		double side1, side2, side3;
+	public:
+		double get_side1()const
+		{
+			return side1;
+		}
+		double get_side2()const
+		{
+			return side2;
+		}
+		double get_side3()const
+		{
+			return side3;
+		}
+		double set_side1(double side1)
+		{
+			if (side1 < 0)side1 = -side1;
+			return this->side1 = side1;
+		}
+		double set_side2(double side2)
+		{
+			if (side2 < 0)side2 = -side2;
+			return this->side2 = side2;
+		}
+		double set_side3(double side3)
+		{
+			if (side3 < 0)side3 = -side3;
+			return this->side3 = side3;
+		}
+
+		Triangle(double side1, double side2, double side3)
+		{
+			set_side1(side1);
+			set_side2(side2);
+			set_side3(side3);
+			if (side3 >= (side1 + side2) || side1 >= (side2 + side3) || side2 >= (side1 + side3))
+				throw exception("Triangle bad side");
+			cout << "TriaConstructor:\t" << this << endl;
+		}
+		~Triangle()
+		{
+			cout << "TriaDestructor:\t" << this << endl;
+		}
+
+		double area()const
+		{
+			return sqrt((perimeter() / 2) * ((perimeter() / 2) - side1) * ((perimeter() / 2) - side2) * ((perimeter() / 2) - side3));
+		}
+		double perimeter()const
+		{
+			return side1+side2+side3;
+		}
+		void draw()const
+		{
+
+		}
+	};
 }
 
 void main()
@@ -137,4 +194,16 @@ void main()
 	cout << "Rectangle area:\t" << rect.area() << endl;
 	cout << "Rectangle perimeter:\t" << rect.perimeter() << endl;
 	rect.draw();
+
+	try 
+	{
+		Geometry::Triangle trgl(5, 5, 5);
+		cout << "Triangle area:\t" << trgl.area() << endl;
+		cout << "Triangle perimeter:\t" << trgl.perimeter() << endl;
+		
+	}
+	catch(const exception& e)
+	{
+		cerr << e.what() << endl;
+	}
 }
